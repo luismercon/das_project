4a4040bf708c1ab3aeb7ada6a3eb7730347ce123
lcvargas <Luis.Mercon@criticalsoftware.com>
2023-12-18 21:41:49 +0000

    First commit

---
+++ src/main/java/pt/isec/mei/das/demo/OnlineCompilationAndBuildSystemApplication.java
@@ -0,0 +1,13 @@
+package pt.isec.mei.das.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCompilationAndBuildSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCompilationAndBuildSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/isec/mei/das/demo/OnlineCompilationAndBuildSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.isec.mei.das.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCompilationAndBuildSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
