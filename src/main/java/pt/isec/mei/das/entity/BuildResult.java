103f74e7f352997bbdd5c6abf760b4e9793a1e4e
lcvargas <Luis.Mercon@criticalsoftware.com>
2024-01-13 14:22:01 +0000

    BuildResult entity

---
+++ src/main/java/pt/isec/mei/das/entity/BuildResult.java
@@ -0,0 +1,48 @@
+package pt.isec.mei.das.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "build_results")
+public class BuildResult {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "build_result_id")
+  private long id;
+
+  @ManyToOne
+  @JoinColumn(name = "project_id", nullable = false)
+  private Project project;
+
+  @Column(name = "source_code_hash")
+  private String sourceCodeHash;
+
+  @Column(name = "compilation_status")
+  private Boolean compilationStatus;
+
+  @Column(name = "error_messages")
+  private String errorMessages;
+
+  @Column(name = "warning_messages")
+  private String warningMessages;
+
+  @Column(name = "executable_file_path")
+  private String executableFilePath;
+
+  @Column(name = "build_logs")
+  private String buildLogs;
+
+  @Column(name = "timestamp")
+  private LocalDateTime timestamp;
+}
--- src/main/java/pt/isec/mei/das/entity/Project.java
+++ src/main/java/pt/isec/mei/das/entity/Project.java
@@ -5,29 +5,31 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.Data;
-
 import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Data;
 
 @Entity
 @Data
 @Table(name = "PROJECT")
 public class Project {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "project_id")
+  private long id;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "project_id")
-    private long id;
+  @Column(name = "name")
+  private String name;
 
-    @Column(name = "name")
-    private String name;
+  @Column(name = "created_at")
+  private LocalDateTime createdAt;
 
-    @Column(name = "created_at")
-    private LocalDateTime createdAt;
+  @Column(name = "file_path")
+  private String filePath;
 
-    @Column(name = "file_path")
-    private String filePath;
-
+  @OneToMany(mappedBy = "project")
+  private List<BuildResult> buildResults;
 }
