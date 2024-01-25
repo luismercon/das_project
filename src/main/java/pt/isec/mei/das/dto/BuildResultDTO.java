2656545ed01c37816cfb949513f8f6238cfc1815
sakumo99 <a2020154261@alumni.iscac.pt>
2024-01-25 10:37:29 +0000

    25-field-masks-allow-to-specify-which-build-result-information-is-retrieved

--- src/main/java/pt/isec/mei/das/dto/BuildResultDTO.java
+++ src/main/java/pt/isec/mei/das/dto/BuildResultDTO.java
@@ -9,7 +9,6 @@ import pt.isec.mei.das.enums.CompilationStatus;
 import java.time.LocalDateTime;
 
 @Data
-@AllArgsConstructor
 @EqualsAndHashCode
 @Builder
 public class BuildResultDTO {
@@ -20,4 +19,21 @@ public class BuildResultDTO {
     private String executableFilePath;
     private String buildLogs;
     private LocalDateTime timestamp;
+
+    // No-argument constructor
+    public BuildResultDTO() {
+    }
+
+    // Existing constructor
+    public BuildResultDTO(Long id, Long projectId, String sourceCodeHash, String compilationStatus, String executableFilePath, String buildLogs, LocalDateTime timestamp) {
+        this.id = id;
+        this.projectId = projectId;
+        this.sourceCodeHash = sourceCodeHash;
+        this.compilationStatus = compilationStatus;
+        this.executableFilePath = executableFilePath;
+        this.buildLogs = buildLogs;
+        this.timestamp = timestamp;
+    }
+
 }
+
